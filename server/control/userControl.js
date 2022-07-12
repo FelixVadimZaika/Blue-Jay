f68bb5275d6da27a02ec46b870e234f8eee273a8
Vadim <melisa228822@gmail.com>
2022-07-13 01:35:05 +0300

    jwt token,bcrypt,registration

--- server/control/userControl.js
+++ server/control/userControl.js
@@ -1,7 +1,28 @@
 const ErrorApi = require('../error/ErrorApi');
+const bcrypt = require('bcrypt')
+const jwt = require('jsonwebtoken')
+const {User,Basket} = require('../models/models')
+const generateJwt = (id,email,role)=>{
+    return jwt.sign({id, email,role},
+        process.env.SECRET_KEY,
+        {expiresIn:'24h'}
+    )
+}
 class UserControl {
     async registration(req,res){
-
+        const{email,password,role} = req.body
+        if(!email || !password){
+            return next(ErrorApi.badRequest('Некоректний email або password'))
+        }
+        const candidate = await User.findOne({where:{email}}) //перевірка чи є такий email
+        if(candidate){
+            return next(ErrorApi.badRequest('Користувач з таким email вже існує'))
+        }
+        const hashPassword = await bcrypt.hash(password,5)
+        const user = await User.create({email,role,password:hashPassword})
+        const basket = await Basket.create({userId:user.id})
+        const token = generateJwt(user.id,user.email,user.role)
+        return res.json({token})
     }
     async login(req,res){
 
